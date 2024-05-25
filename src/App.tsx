417a256921128ee1d31293db42bca8c304773e9c
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-05-25 12:05:17 +0200

    Main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
-export function App() {
-  return <></>;
-}
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Button } from "./components/Button";
+
+export const App = () => {
+  const [showSideNav, setShowSideNav] = useState(false);
+
+  return (
+    <div className="flex h-screen divide-x divide-gray-200 overflow-auto text-gray-900">
+      <div className="relative min-w-[65%] w-full bg-white p-2">
+        <Button
+          className="absolute right-0 top-0 h-14 rounded-none rounded-es-lg p-0 lg:hidden"
+          onClick={() => setShowSideNav(true)}
+          aria-label="Open navigation bar"
+        >
+          <ChevronLeft aria-hidden />
+        </Button>
+      </div>
+      <div
+        className={`${
+          showSideNav ? "" : "hidden"
+        } absolute right-0 top-0 z-10 h-screen w-[--size] bg-white p-2 [--size:min(theme(spacing.96),90vw)] lg:sticky lg:top-0 lg:block lg:w-auto lg:min-w-[35%]`}
+      >
+        <Button
+          className="absolute right-[--size] top-0 h-14 rounded-none rounded-es-lg p-0 lg:hidden"
+          onClick={() => setShowSideNav(false)}
+          aria-label="Close navigation bar"
+        >
+          <ChevronRight />
+        </Button>
+        <div className="flex justify-between">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import { forwardRef, ComponentPropsWithoutRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+export type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  secondary?: boolean;
+};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props: ButtonProps, ref) => {
+    return (
+      <button
+        {...props}
+        ref={ref}
+        className={twMerge(
+          "rounded-lg border px-2 py-1 font-bold transition-[background-color] disabled:cursor-not-allowed disabled:opacity-70",
+          props.secondary
+            ? "border-gray-400 bg-gray-200 [&:not(:disabled):hover]:bg-gray-300"
+            : "border-lime-500 bg-lime-300 [&:not(:disabled):hover]:bg-lime-400",
+          props.className
+        )}
+      >
+        {props.children}
+      </button>
+    );
+  }
+);
