4b393133e16bfa5fe4987eba8dfda5406d266a93
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-05-31 12:54:47 +0200

    Set base URL in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), topLevelAwait()],
+  base: "/roarer-2",
 });
