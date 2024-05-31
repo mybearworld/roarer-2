1c83828dbddc497d91a14a119a80c439fe030c58
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-05-31 12:46:58 +0200

    Don't hide button to close the side nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { twMerge } from "tailwind-merge";
 import { Account } from "./components/Account";
 import { Chats } from "./components/Chats";
 import { Button } from "./components/Button";
@@ -10,29 +11,29 @@ export const App = () => {
   const [chat, setChat] = useState("home");
 
   return (
-    <div className="flex h-screen max-h-screen divide-x divide-gray-200 overflow-auto text-gray-900">
+    <div className="flex h-screen max-h-screen divide-x divide-gray-200 overflow-auto text-gray-900 [--nav-bar-size:min(theme(spacing.96),90vw)]">
       <div className="relative max-h-screen w-full min-w-[65%] overflow-auto bg-white p-2">
         <Button
-          className="absolute right-0 top-0 h-14 rounded-none rounded-es-lg p-0 lg:hidden"
-          onClick={() => setShowSideNav(true)}
+          className={twMerge(
+            "absolute top-0 h-14 rounded-none rounded-es-lg p-0 lg:hidden",
+            showSideNav ? "right-[--nav-bar-size]" : "right-0",
+          )}
+          onClick={() => setShowSideNav((n) => !n)}
           aria-label="Open navigation bar"
         >
-          <ChevronLeft aria-hidden />
+          {showSideNav ? (
+            <ChevronRight aria-hidden />
+          ) : (
+            <ChevronLeft aria-hidden />
+          )}
         </Button>
         <Posts chat={chat} />
       </div>
       <div
         className={`${
           showSideNav ? "" : "hidden"
-        } absolute right-0 top-0 z-10 h-screen max-h-screen w-[--size] overflow-auto bg-white py-2 [--size:min(theme(spacing.96),90vw)] lg:sticky lg:top-0 lg:block lg:w-auto lg:min-w-[35%]`}
+        } absolute right-0 top-0 z-10 h-screen max-h-screen w-[--nav-bar-size] overflow-auto bg-white py-2 lg:sticky lg:top-0 lg:block lg:w-auto lg:min-w-[35%]`}
       >
-        <Button
-          className="absolute right-[--size] top-0 h-14 rounded-none rounded-es-lg p-0 lg:hidden"
-          onClick={() => setShowSideNav(false)}
-          aria-label="Close navigation bar"
-        >
-          <ChevronRight />
-        </Button>
         <div className="mb-2 flex justify-between px-2">
           <div></div>
           <div>
