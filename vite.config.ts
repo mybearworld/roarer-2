37d0f32d8486817db2f01cae281f10b246617570
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-06-02 13:30:45 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), topLevelAwait()],
+  plugins: [
+    react(),
+    topLevelAwait(),
+    VitePWA({
+      manifest: {
+        name: "Roarer",
+        short_name: "Roarer",
+        description: "A simple Meower client.",
+        icons: [
+          {
+            src: "./bear.svg",
+            sizes: "any",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/roarer-2",
 });
