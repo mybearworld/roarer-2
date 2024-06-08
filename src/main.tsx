fa5cc36c4f24eea0d307924b172bd8aeeb645a5f
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-06-08 10:01:14 +0200

    Log warning message to the console

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>,
 );
+
+console.log("%cWarning!", "font-size: 22px; font-weight: bold; color: red;");
+console.log(
+  "This console is intended for developers. If you were told to paste code in here, %cdon't do it!%c It could allow someone to do anything to your Meower account, including getting it banned.",
+  "font-style: italic",
+  "font-style: none",
+);
+console.log(
+  "%c(For Roarer's source code, see https://github.com/mybearworld/roarer-2)",
+  "font-size: 10px",
+);
