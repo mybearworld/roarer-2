01fa9aa828fa8c20a70db991a6b7e13e5e5e7d7c
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-06-13 16:12:44 +0200

    Switch markdown library to marked-react

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,35 +1,19 @@
-import { createContext, useContext, useState } from "react";
-import ReactMarkdown from "react-markdown";
-import remarkBreaks from "remark-breaks";
-import remarkGfm from "remark-gfm";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import Marked from "marked-react";
 import { codeToHtml } from "shiki";
-import { urlFromDiscordEmoji } from "../lib/discordEmoji";
+// import { urlFromDiscordEmoji } from "../lib/discordEmoji";
 import { hostWhitelist } from "../lib/hostWhitelist";
 import { User } from "./User";
 
-const IsPreContext = createContext(false);
-const DISALLOWED_INLINE = [
-  "blockquote",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "hr",
-  "img",
-  "input",
-  "li",
-  "ol",
-  "pre",
-  "table",
-  "tbody",
-  "td",
-  "th",
-  "thead",
-  "tr",
-  "ul",
-];
+const HEADING_TO_SIZE = {
+  1: "text-2xl",
+  2: "text-xl",
+  3: "text-lg",
+  4: "text-md",
+  5: "text-sm",
+  6: "text-xs",
+} as const;
 
 export type MarkdownProps = {
   children: string;
@@ -39,144 +23,128 @@ export type MarkdownProps = {
 export const Markdown = (mdProps: MarkdownProps) => {
   const md = mdProps.children;
   return (
-    <ReactMarkdown
-      remarkPlugins={[remarkGfm, remarkBreaks]}
-      disallowedElements={mdProps.inline ? DISALLOWED_INLINE : []}
-      unwrapDisallowed
-      components={{
-        a: (props) => {
-          const match = props.href?.match(
+    <Marked
+      gfm
+      breaks
+      isInline={!!mdProps.inline}
+      langPrefix=""
+      renderer={{
+        code: (code, lang) => {
+          const [syntaxHighlighted, setSyntaxHighlighted] = useState("");
+          if (lang) {
+            codeToHtml(code?.toString() ?? "", {
+              lang,
+              theme: "github-dark-default",
+              structure: "inline",
+            }).then(setSyntaxHighlighted);
+          }
+          return (
+            <pre className="overflow-auto rounded-lg bg-gray-800 px-1 py-0.5 text-gray-100">
+              {syntaxHighlighted ? (
+                <code dangerouslySetInnerHTML={{ __html: syntaxHighlighted }} />
+              ) : (
+                <code>{code}</code>
+              )}
+            </pre>
+          );
+        },
+        blockquote: (children) => (
+          <blockquote className="border-l-2 border-lime-500 pl-2 dark:border-lime-600">
+            {children}
+          </blockquote>
+        ),
+        heading: (children, level) => (
+          <p className={twMerge("font-bold", HEADING_TO_SIZE[level])}>
+            {children}
+          </p>
+        ),
+        hr: () => <hr className="mx-12 my-2 border-current opacity-20" />,
+        list: (children, ordered) => {
+          const Tag = ordered ? "ol" : "ul";
+          return (
+            <Tag
+              className={twMerge(
+                "table border-spacing-x-1 list-inside",
+                ordered ? "list-decimal" : "list-disc",
+              )}
+            >
+              {children}
+            </Tag>
+          );
+        },
+        listItem: (children) => (
+          <li className="table-row">
+            <span className="table-cell text-right">
+              <span className="list-item" />
+            </span>
+            <div className="table-cell">{children}</div>
+          </li>
+        ),
+        checkbox: (checked) => (
+          <>
+            <input
+              className="mr-2"
+              type="checkbox"
+              checked={!!checked}
+              readOnly
+              aria-hidden
+            />
+            <span className="sr-only">{checked ? "Done" : "Not done"}</span>
+          </>
+        ),
+        paragraph: (children) => (
+          <p className="my-1 first:mt-0 last:mb-0">{children}</p>
+        ),
+        table: (children) => (
+          <table className="border-collapse">{children}</table>
+        ),
+        tableCell: (children, flags) => {
+          const Tag = flags.header ? "th" : "td";
+          return (
+            <Tag
+              className="border border-gray-300 px-2"
+              style={{ textAlign: flags.align ?? "left" }}
+            >
+              {children}
+            </Tag>
+          );
+        },
+        link: (href, text) => {
+          const match = href.match(
             /^https?:\/\/app.meower.org\/users\/([a-z0-9\-_]+)$/i,
           );
           if (match) {
             const username = match[1]!;
             return (
               <User username={username}>
-                <button
-                  type="button"
-                  title={props.title}
-                  className="font-bold text-lime-600"
-                >
-                  {props.children}
+                <button type="button" className="font-bold text-lime-600">
+                  {text}
                 </button>
               </User>
             );
           }
           return (
-            <a
-              href={props.href}
-              title={props.title}
-              className="font-bold text-lime-600"
-            >
-              {props.children}
+            <a href={href} className="font-bold text-lime-600">
+              {text}
             </a>
           );
         },
-        blockquote: (props) => (
-          <blockquote className="border-l-2 border-lime-500 pl-2 dark:border-lime-600">
-            {props.children}
-          </blockquote>
-        ),
-        code: (props) => {
-          const isPre = useContext(IsPreContext);
-          const [code, setCode] = useState("");
-          const language = props.className?.replace("language-", "");
-          if (language) {
-            codeToHtml(props.children?.toString() ?? "", {
-              lang: language,
-              theme: "github-dark-default",
-              structure: "inline",
-            }).then(setCode);
-          }
-          return isPre ? (
-            <pre
-              className={
-                "overflow-auto rounded-lg bg-gray-800 px-1 py-0.5 text-gray-100"
-              }
-            >
-              {code ? (
-                <code dangerouslySetInnerHTML={{ __html: code }} />
-              ) : (
-                <code>{props.children}</code>
-              )}
-            </pre>
-          ) : (
-            <code className="rounded-lg bg-gray-800 px-1 py-0.5 text-gray-100">
-              {props.children}
-            </code>
-          );
-        },
-        h1: (props) => <p className="text-2xl font-bold">{props.children}</p>,
-        h2: (props) => <p className="text-xl font-bold">{props.children}</p>,
-        h3: (props) => <p className="text-lg font-bold">{props.children}</p>,
-        h4: (props) => <p className="text-lg font-bold">{props.children}</p>,
-        h5: (props) => <p className="text-md font-bold">{props.children}</p>,
-        h6: (props) => <p className="text-sm font-bold">{props.children}</p>,
-        hr: () => <hr className="mx-12 my-2 border-current opacity-20" />,
-        img: (props) =>
-          hostWhitelist.some((host) => props.src?.startsWith(host)) ? (
+        image: (src, alt, title) =>
+          hostWhitelist.some((host) => src.startsWith(host)) ? (
             <img
-              src={props.src}
-              alt={props.alt}
-              title={props.title}
+              src={src}
+              alt={alt}
+              title={title ?? ""}
               className="inline-block"
             />
           ) : (
-            <a className="font-bold text-lime-600" href={props.src}>
-              {props.alt || "Unnamed image"}
+            <a className="font-bold text-lime-600" href={src}>
+              {alt || "Unnamed image"}
             </a>
           ),
-        input: (props) => (
-          <>
-            <input type="checkbox" checked={props.checked} aria-hidden />
-            <span className="sr-only">
-              {props.checked ? "Done" : "Not done"}
-            </span>
-          </>
-        ),
-        ol: (props) => (
-          <ol
-            className="table border-spacing-x-1 list-inside list-decimal"
-            start={props.start}
-          >
-            {props.children}
-          </ol>
-        ),
-        li: (props) => (
-          <li className="table-row">
-            <span className="table-cell text-right">
-              <span className="list-item" />
-            </span>
-            <div className="table-cell">{props.children}</div>
-          </li>
-        ),
-        p: (props) => (
-          <p className="my-1 first:mt-0 last:mb-0">{props.children}</p>
-        ),
-        pre: (props) => (
-          <IsPreContext.Provider value={true}>
-            {props.children}
-          </IsPreContext.Provider>
-        ),
-        table: (props) => (
-          <table className="border-collapse">{props.children}</table>
-        ),
-        td: (props) => (
-          <td className="border border-gray-300 px-2" style={props.style}>
-            {props.children}
-          </td>
-        ),
-        th: (props) => (
-          <th className="border border-gray-300 px-2" style={props.style}>
-            {props.children}
-          </th>
-        ),
-        ul: (props) => (
-          <ul className="table list-inside list-disc">{props.children}</ul>
-        ),
       }}
     >
       {md}
-    </ReactMarkdown>
+    </Marked>
   );
 };
