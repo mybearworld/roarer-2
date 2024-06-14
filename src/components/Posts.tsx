mybearworld/roarer-2

commit 5918b3bb36a8127791f3a3aa84a1a82463531d89

Optimistically post

src/components/Posts.tsx
@@ -118,10 +118,13 @@ type EnterPostProps = {
 const EnterPost = (props: EnterPostProps) => {
   const post = useAPI((state) => state.post);
   const handleSubmit = (postContent: string, attachments: string[]) => {
-    return post(postContent, props.chat, attachments);
+    post(postContent, props.chat, attachments);
+    return Promise.resolve({ error: false } as const);
   };
 
-  return <EnterPostBase {...props} onSubmit={handleSubmit} />;
+  return (
+    <EnterPostBase {...props} onSubmit={handleSubmit} dontDisableWhenPosting />
+  );
 };
 
 export type EnterPostBaseProps = {
@@ -130,6 +133,7 @@ export type EnterPostBaseProps = {
   removeReply?: () => void;
   basePostContent?: string;
   onSuccess?: () => void;
+  dontDisableWhenPosting?: boolean;
   onSubmit: (
     postContent: string,
     attachments: string[],
@@ -220,7 +224,7 @@ export const EnterPostBase = (props: EnterPostBaseProps) => {
         value={postContent}
         onChange={(e) => setPostContent(e.currentTarget.value)}
         onInput={() => sendTyping(props.chat)}
-        disabled={state !== "writing"}
+        disabled={!props.dontDisableWhenPosting && state !== "writing"}
         onEnter={handlePost}
         before={
           <>

src/components/Post.tsx
@@ -113,6 +113,7 @@ const PostBase = memo((props: PostBaseProps) => {
     >
       <SpeechBubble
         reply={props.reply}
+        transparent={!!props.post.optimistic}
         speaker={
           <User username={props.post.u}>
             <button>
@@ -141,7 +142,7 @@ const PostBase = memo((props: PostBaseProps) => {
                   {props.post.u}
                 </button>
               </User>
-              {!props.reply ? (
+              {!props.reply && !props.post.optimistic ? (
                 <div className="flex gap-2">
                   {credentials?.username === props.post.u ? (
                     <button
@@ -164,6 +165,11 @@ const PostBase = memo((props: PostBaseProps) => {
                 </div>
               ) : undefined}
             </div>
+            {props.post.optimistic?.error ? (
+              <div className="text-red-500">
+                This post failed sending. Message: {props.post.optimistic.error}
+              </div>
+            ) : undefined}
             {!props.reply && reply?.id ? (
               <div className="mt-1">
                 <Post id={reply.id} reply />
@@ -209,10 +215,13 @@ type SpeechBubbleProps = {
   reply?: boolean | "topLevel";
   speaker: ReactNode;
   bubble: ReactNode;
+  transparent?: boolean;
 };
 const SpeechBubble = (props: SpeechBubbleProps) => {
   return (
-    <div className="flex gap-3">
+    <div
+      className={twMerge("flex gap-3", props.transparent ? "opacity-70" : "")}
+    >
       <div>{props.speaker}</div>
       <div
         className={twMerge(

src/lib/api/posts.ts
@@ -16,7 +16,9 @@ const ATTACHMENT_SCHEMA = z.object({
   width: z.number(),
 });
 
-export type Post = z.infer<typeof POST_SCHEMA>;
+export type Post = z.infer<typeof POST_SCHEMA> & {
+  optimistic?: { error?: string };
+};
 export const POST_SCHEMA = z.object({
   attachments: ATTACHMENT_SCHEMA.array(),
   edited_at: z.number().optional(),
@@ -57,7 +59,11 @@ const POST_PACKET_SCHEMA = z.object({
 export type PostsSlice = {
   chatPosts: Record<
     string,
-    Errorable<{ posts: string[]; stopLoadingMore: boolean }>
+    Errorable<{
+      posts: string[];
+      stopLoadingMore: boolean;
+      currentOptimistics: Record<string, string>;
+    }>
   >;
   posts: Record<string, Errorable<Post | { isDeleted: true }>>;
   addPost: (post: Post) => void;
@@ -77,7 +83,7 @@ export type PostsSlice = {
     content: string,
     chat: string,
     attachments?: string[],
-  ) => Promise<{ error: true; message: string } | { error: false }>;
+  ) => Promise<void>;
   editPost: (
     id: string,
     newContent: string,
@@ -112,6 +118,15 @@ export const createPostsSlice: Slice<PostsSlice> = (set, get) => {
           return;
         }
         chatPosts.posts.unshift(post.post_id);
+        if (post.u === draft.credentials?.username) {
+          const id = Object.entries(chatPosts.currentOptimistics).find(
+            ([_id, optimisticPostContent]) => optimisticPostContent === post.p,
+          )?.[0];
+          if (!id) {
+            return;
+          }
+          draft.posts[id] = { error: false, isDeleted: true };
+        }
       });
     });
     cloudlink.on("direct", (packet: unknown) => {
@@ -135,12 +150,20 @@ export const createPostsSlice: Slice<PostsSlice> = (set, get) => {
     });
   });
 
+  let id = 0;
+  const getOptimisticId = () => `optimistic:${id++}`;
+
   const loadingPosts = new Set<string>();
   const loadingChats = new Set<string>();
   return {
     posts: {},
     chatPosts: {
-      livechat: { posts: [], stopLoadingMore: true, error: false },
+      livechat: {
+        posts: [],
+        stopLoadingMore: true,
+        currentOptimistics: {},
+        error: false,
+      },
     },
     addPost: (post: Post) => {
       set((draft) => {
@@ -185,6 +208,7 @@ export const createPostsSlice: Slice<PostsSlice> = (set, get) => {
         draft.chatPosts[id] = {
           posts: [...(posts?.posts ?? []), ...response.posts],
           stopLoadingMore: response.stop,
+          currentOptimistics: {},
           error: false,
         };
       });
@@ -227,6 +251,31 @@ export const createPostsSlice: Slice<PostsSlice> = (set, get) => {
     },
     post: async (content, chat, attachments) => {
       const state = get();
+      const optimisticId = getOptimisticId();
+      const credentials = state.credentials;
+      if (!credentials) {
+        return;
+      }
+      set((draft) => {
+        const trimmedContent = content.trim();
+        draft.posts[optimisticId] = {
+          type: 1,
+          attachments: [],
+          isDeleted: false,
+          p: trimmedContent,
+          post_id: optimisticId,
+          post_origin: chat,
+          t: { e: Date.now() },
+          u: credentials.username,
+          error: false,
+          optimistic: {},
+        };
+        const chatPosts = draft.chatPosts[chat];
+        if (chatPosts && !chatPosts.error) {
+          chatPosts.posts.unshift(optimisticId);
+          chatPosts.currentOptimistics[optimisticId] = trimmedContent;
+        }
+      });
       const response = await request(
         fetch(
           `https://api.meower.org/${chat === "home" ? "home" : `posts/${encodeURIComponent(chat)}`}`,
@@ -241,10 +290,16 @@ export const createPostsSlice: Slice<PostsSlice> = (set, get) => {
         ),
         POST_SCHEMA,
       );
-      if (response.error) {
-        return response;
+      if (!response.error) {
+        return;
       }
-      return { error: false };
+      set((draft) => {
+        const post = draft.posts[optimisticId];
+        if (!post || post.error || post.isDeleted) {
+          return;
+        }
+        post.optimistic = { error: response.message };
+      });
     },
     editPost: (id, newContent) => {
       const state = get();

commit 1b38997d6a48f759aac79f6518ca656a6a07bb5e

Disable the post input when uploading again
Regression from 5918b3bb36a8127791f3a3aa84a1a82463531d89

src/components/Posts.tsx
@@ -235,7 +235,10 @@ export const EnterPostBase = (props: EnterPostBaseProps) => {
         value={postContent}
         onChange={(e) => setPostContent(e.currentTarget.value)}
         onInput={() => sendTyping(props.chat)}
-        disabled={!props.dontDisableWhenPosting && state !== "writing"}
+        disabled={
+          state === "uploading" ||
+          (!props.dontDisableWhenPosting && state !== "posting")
+        }
         onEnter={handlePost}
         before={
           <>