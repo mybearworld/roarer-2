272f0b2db3230d82169b1a445c85ff5287509b9c
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-07-18 16:39:24 +0200

    Fix dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/roarer-2",
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
   build: {
     target: "esnext"
   }
